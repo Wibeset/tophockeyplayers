14d96b20e7ecba20f4078c25c2049d69d451671f
Dominic Martineau <dominic.martineau@ellipse-synergie.com>
2016-06-09 02:21:00 +0000

    Add defensemen and goalies 2015-16 statistics

--- config/players.php
+++ config/players.php
@@ -1,382 +1,104 @@
 <?php
 
 return [
-
-    [
-        'id' => '8473994',
-        'name' => 'J. Benn',
-    ],
-    [
-        'id' => '8475166',
-        'name' => 'J. Tavares',
-    ],
-    [
-        'id' => '8471675',
-        'name' => 'S. Crosby',
-    ],
-    [
-        'id' => '8471214',
-        'name' => 'A. Ovechkin',
-    ],
-    [
-        'id' => '8474161',
-        'name' => 'J. Voracek',
-    ],
-    [
-        'id' => '8473563',
-        'name' => 'N. Backstrom',
-    ],
-    [
-        'id' => '8475794',
-        'name' => 'T. Seguin',
-    ],
-    [
-        'id' => '8470201',
-        'name' => 'J. Hudler',
-    ],
-    [
-        'id' => '8467875',
-        'name' => 'D. Sedin',
-    ],
-    [
-        'id' => '8475765',
-        'name' => 'V. Tarasenko',
-    ],
-    [
-        'id' => '8470612',
-        'name' => 'R. Getzlaf',
-    ],
-    [
-        'id' => '8473512',
-        'name' => 'C. Giroux',
-    ],
-    [
-        'id' => '8470621',
-        'name' => 'C. Perry',
-    ],
-    [
-        'id' => '8473548',
-        'name' => 'P. Kessel',
-    ],
-    [
-        'id' => '8475791',
-        'name' => 'T. Hall',
-    ],
-    [
-        'id' => '8470794',
-        'name' => 'J. Pavelski',
-    ],
-    [
-        'id' => '8466378',
-        'name' => 'M. St. Louis'
-    ],
-    [
-        'id' => '8474564',
-        'name' => 'S. Stamkos'
-    ],
-    [
-        'id' => '8474141',
-        'name' => 'P. Kane'
-    ],
-    [
-        'id' => '8470595',
-        'name' => 'E. Staal'
-    ],
-    [
-        'id' => '8470543',
-        'name' => 'C. Kunitz'
-    ],
-    [
-        'id' => '8467514',
-        'name' => 'P. Datsyuk'
-    ],
-    [
-        'id' => '8471215',
-        'name' => 'E. Malkin'
-    ],
-    [
-        'id' => '8469455',
-        'name' => 'J. Spezza'
-    ],
-    [
-        'id' => '8469454',
-        'name' => 'I. Kovalchuk'
-    ],
-    [
-        'id' => '8467876',
-        'name' => 'H. Sedin'
-    ],
-    [
-        'id' => '8460542',
-        'name' => 'P. Elias'
-    ],
-    [
-        'id' => '8471707',
-        'name' => 'J. Neal'
-    ],
-    [
-        'id' => '8462042',
-        'name' => 'J. Iginla'
-    ],
-    [
-        'id' => '8457981',
-        'name' => 'T. Selanne'
-    ],
-    [
-        'id' => '8468083',
-        'name' => 'H. Zetterberg'
-    ],
-    [
-        'id' => '8467389',
-        'name' => 'B. Richards'
-    ],
-    [
-        'id' => '8474578',
-        'name' => 'E. Karlsson'
-    ],
-    [
-        'id' => '8470281',
-        'name' => 'D. Keith'
-    ],
-    [
-        'id' => '8470642',
-        'name' => 'S. Weber'
-    ],
-    [
-        'id' => '8470834',
-        'name' => 'D. Bufyglien'
-    ],
-    [
-        'id' => '8475167',
-        'name' => 'V. Hedman'
-    ],
-    [
-        'id' => '8474056',
-        'name' => 'P.K. Subban'
-    ],
-    [
-        'id' => '8471735',
-        'name' => 'K. Yandle'
-    ],
-    [
-        'id' => '8474565',
-        'name' => 'A. Pietrangelo'
-    ],
-    [
-        'id' => '8470222',
-        'name' => 'J. Wisniewski'
-    ],
-    [
-        'id' => '8468509',
-        'name' => 'N. Kronwall'
-    ],
-    [
-        'id' => '8470613',
-        'name' => 'B. Burns'
-    ],
-    [
-        'id' => '8469770',
-        'name' => 'D. Wideman'
-    ],
-    [
-        'id' => '8474600',
-        'name' => 'R. Josi'
-    ],
-    [
-        'id' => '8474590',
-        'name' => 'J. Carlson'
-    ],
-    [
-        'id' => '8471724',
-        'name' => 'K. Letang'
-    ],
-    [
-        'id' => '8475197',
-        'name' => 'T. Barrie'
-    ],
-    [
-        'id' => '8468309',
-        'name' => 'M. Streit'
-    ],
-    [
-        'id' => '8470600',
-        'name' => 'R. Suter'
-    ],
-    [
-        'id' => '8467496',
-        'name' => 'A. Markov'
-    ],
-    [
-        'id' => '8459670',
-        'name' => 'K. Timonen'
-    ],
-    [
-        'id' => '8471742',
-        'name' => 'C. Franson'
-    ],
-    [
-        'id' => '8470602',
-        'name' => 'D. Phaneuf'
-    ],
-    [
-        'id' => '8466285',
-        'name' => 'B. Campbell'
-    ],
-    [
-        'id' => '8465009',
-        'name' => 'Z. Chara'
-    ],
-    [
-        'id' => '8471303',
-        'name' => 'A. Edler'
-    ],
-    [
-        'id' => '8467096',
-        'name' => 'D. Boyle'
-    ],
-    [
-        'id' => '8468598',
-        'name' => 'L. Visnovsky'
-    ],
-    [
-        'id' => '8457063',
-        'name' => 'N. Lidstrom'
-    ],
-    [
-        'id' => '8470828',
-        'name' => 'T. Enstrom'
-    ],
-    [
-        'id' => '8469555',
-        'name' => 'C. Ehrhoff'
-    ],
-    [
-        'id' => '8471679',
-        'name' => 'C. Price'
-    ],
-    [
-        'id' => '8471469',
-        'name' => 'P. Rinne'
-    ],
-    [
-        'id' => '8474651',
-        'name' => 'B. Holtby'
-    ],
-    [
-        'id' => '8471750',
-        'name' => 'B. Bishop'
-    ],
-    [
-        'id' => '8470860',
-        'name' => 'J. Halak'
-    ],
-    [
-        'id' => '8470860',
-        'name' => 'D. Dubnyk'
-    ],
-    [
-        'id' => '8471734',
-        'name' => 'J. Quick'
-    ],
-    [
-        'id' => '8475883',
-        'name' => 'F. Andersen'
-    ],
-    [
-        'id' => '8470140',
-        'name' => 'K. Lehtonen'
-    ],
-    [
-        'id' => '8470594',
-        'name' => 'M-C. Fleury'
-    ],
-    [
-        'id' => '8473575',
-        'name' => 'S. Varlamov'
-    ],
-    [
-        'id' => '8474550',
-        'name' => 'A. Niemi'
-    ],
-    [
-        'id' => '8471695',
-        'name' => 'T. Rask'
-    ],
-    [
-        'id' => '8473461',
-        'name' => 'S. Mason'
-    ],
-    [
-        'id' => '8468685',
-        'name' => 'H. Lundqvist'
-    ],
-    [
-        'id' => '8470645',
-        'name' => 'C. Crawford'
-    ],
-    [
-        'id' => '8473404',
-        'name' => 'N. Backstrom'
-    ],
-    [
-        'id' => '8460705',
-        'name' => 'E. Nabokov'
-    ],
-    [
-        'id' => '8475683',
-        'name' => 'S. Bobrovsky'
-    ],
-    [
-        'id' => '8470657',
-        'name' => 'J. Howard'
-    ],
-    [
-        'id' => '8471715',
-        'name' => 'O. Pavelec'
-    ],
-    [
-        'id' => '8469608',
-        'name' => 'M. Smith'
-    ],
-    [
-        'id' => '8460703',
-        'name' => 'T. Thomas'
-    ],
-    [
-        'id' => '8462147',
-        'name' => 'M. Kiprusoff'
-    ],
-    [
-        'id' => '8468524',
-        'name' => 'I. Bryzgalov'
-    ],
-    [
-        'id' => '8466141',
-        'name' => 'R. Luongo'
-    ],
-    [
-        'id' => '8470320',
-        'name' => 'C. Ward'
-    ],
-    [
-        'id' => '8466138',
-        'name' => 'J. Thornton'
-    ],
-    [
-        'id' => '8476346',
-        'name' => 'J. Gaudreau'
-    ],
-    [
-        'id' => '8471218',
-        'name' => 'B. Wheeler'
-    ],
-    [
-        'id' => '8475744',
-        'name' => 'E. Kuznetsov'
-    ],
-    [
-        'id' => '8478550',
-        'name' => 'A. Panarin'
-    ]
-
+    '8473994' => 'J. Benn',
+    '8475166' => 'J. Tavares',
+    '8471675' => 'S. Crosby',
+    '8471214' => 'A. Ovechkin',
+    '8474161' => 'J. Voracek',
+    '8473563' => 'N. Backstrom',
+    '8475794' => 'T. Seguin',
+    '8470201' => 'J. Hudler',
+    '8467875' => 'D. Sedin',
+    '8475765' => 'V. Tarasenko',
+    '8470612' => 'R. Getzlaf',
+    '8473512' => 'C. Giroux',
+    '8470621' => 'C. Perry',
+    '8473548' => 'P. Kessel',
+    '8475791' => 'T. Hall',
+    '8470794' => 'J. Pavelski',
+    '8466378' => 'M. St. Louis',
+    '8474564' => 'S. Stamkos',
+    '8474141' => 'P. Kane',
+    '8470595' => 'E. Staal',
+    '8470543' => 'C. Kunitz',
+    '8467514' => 'P. Datsyuk',
+    '8471215' => 'E. Malkin',
+    '8469455' => 'J. Spezza',
+    '8469454' => 'I. Kovalchuk',
+    '8467876' => 'H. Sedin',
+    '8460542' => 'P. Elias',
+    '8471707' => 'J. Neal',
+    '8462042' => 'J. Iginla',
+    '8457981' => 'T. Selanne',
+    '8468083' => 'H. Zetterberg',
+    '8467389' => 'B. Richards',
+    '8474578' => 'E. Karlsson',
+    '8470281' => 'D. Keith',
+    '8470642' => 'S. Weber',
+    '8470834' => 'D. Bufyglien',
+    '8475167' => 'V. Hedman',
+    '8474056' => 'P.K. Subban',
+    '8471735' => 'K. Yandle',
+    '8474565' => 'A. Pietrangelo',
+    '8470222' => 'J. Wisniewski',
+    '8468509' => 'N. Kronwall',
+    '8470613' => 'B. Burns',
+    '8469770' => 'D. Wideman',
+    '8474600' => 'R. Josi',
+    '8474590' => 'J. Carlson',
+    '8471724' => 'K. Letang',
+    '8475197' => 'T. Barrie',
+    '8468309' => 'M. Streit',
+    '8470600' => 'R. Suter',
+    '8467496' => 'A. Markov',
+    '8459670' => 'K. Timonen',
+    '8471742' => 'C. Franson',
+    '8470602' => 'D. Phaneuf',
+    '8466285' => 'B. Campbell',
+    '8465009' => 'Z. Chara',
+    '8471303' => 'A. Edler',
+    '8467096' => 'D. Boyle',
+    '8468598' => 'L. Visnovsky',
+    '8457063' => 'N. Lidstrom',
+    '8470828' => 'T. Enstrom',
+    '8469555' => 'C. Ehrhoff',
+    '8471679' => 'C. Price',
+    '8471469' => 'P. Rinne',
+    '8474651' => 'B. Holtby',
+    '8471750' => 'B. Bishop',
+    '8470860' => 'J. Halak',
+    '8470860' => 'D. Dubnyk',
+    '8471734' => 'J. Quick',
+    '8475883' => 'F. Andersen',
+    '8470140' => 'K. Lehtonen',
+    '8470594' => 'M-C. Fleury',
+    '8473575' => 'S. Varlamov',
+    '8474550' => 'A. Niemi',
+    '8471695' => 'T. Rask',
+    '8473461' => 'S. Mason',
+    '8468685' => 'H. Lundqvist',
+    '8470645' => 'C. Crawford',
+    '8473404' => 'N. Backstrom',
+    '8460705' => 'E. Nabokov',
+    '8475683' => 'S. Bobrovsky',
+    '8470657' => 'J. Howard',
+    '8471715' => 'O. Pavelec',
+    '8469608' => 'M. Smith',
+    '8460703' => 'T. Thomas',
+    '8462147' => 'M. Kiprusoff',
+    '8468524' => 'I. Bryzgalov',
+    '8466141' => 'R. Luongo',
+    '8470320' => 'C. Ward',
+    '8466138' => 'J. Thornton',
+    '8476346' => 'J. Gaudreau',
+    '8471218' => 'B. Wheeler',
+    '8475744' => 'E. Kuznetsov',
+    '8478550' => 'A. Panarin',
+    '8475906' => 'J. Klingberg',
+    '8470966' => 'M. Giordano',
+    '8475171' => 'O. E.-Larsson',
+    '8474563' => 'D. Doughty',
+    '8471227' => 'D. Dubnyk',
+    '8474889' => 'M. Jones',
 ];
